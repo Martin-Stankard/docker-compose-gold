9f85c4c36bfbca824dcf5acb15d75e32790e7354
Martin S <martin.stankard@hotmail.com>
2025-01-18 20:33:00 -0700

    aok

---
+++ localSrc/reflexive/Readme.md
@@ -0,0 +1,6 @@
+https://pypi.org/project/pytest-docker/
+
+
+Why this AND pytest service? 
+- This will run ```docker compose up``` + tests on THAT instance of docker-compose-golden (inside a running copy of ```docker compose --profile reflexive up``` , so meta ... )
+
---
+++ localSrc/reflexive/src/testDcReflex.py
@@ -0,0 +1,18 @@
+#pytest
+
+
+print("hello from reflexive land")
+
+#run docker compose....
+
+#maybe a check on containers up and running test every five seconds until ALL UP
+
+#Begin tests
+
+
+# brute force
+
+
+
+
+#finally kick of "normal, non reflexive testing" in pytest, across golden-internal-network/ and golden-external-network/
